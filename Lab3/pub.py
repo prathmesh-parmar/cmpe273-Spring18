221131bc263b7d678674b6f87ef6deeb735380a5
prathmesh-parmar <31287314+prathmesh-parmar@users.noreply.github.com>
2018-03-10 21:20:49 -0800

    Update

--- Lab3/pub.py
+++ Lab3/pub.py
@@ -8,12 +8,13 @@ context = zmq.Context()
 pub = context.socket(zmq.PUB)
 pull = context.socket(zmq.PULL)
 pull.bind("tcp://127.0.0.1:5680")
+
 pub.bind("tcp://127.0.0.1:5681")
 
 while True:
     time.sleep(1)
-    data=pull.recv_json()
-    print(data)
-    print(data['name'])
-    message = "1 >> {name} >> {message}".format(name=data['name'],message=data['message'])
-    pub.send_string(message)
+    data=pull.recv_string()
+    data_1 = data.split(",")
+    print(data_1)
+    pub.send_string(data)
+    print("sent")
